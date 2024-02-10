561d89c86ca2079f6ecc63388a11baa573069f69
Vladimir Kolchurin <18503099+kolchurinvv@users.noreply.github.com>
2024-02-10 22:14:04 +0100

    file structure makes more sence to me now

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,6 @@
-mod data;
+pub mod data {
+    pub mod categories;
+}
 mod web;
+
+pub use data::categories::{construct_url, seed};
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-mod data;
+// mod data;
 // use std::collections::HashMap;
 
+use corrosion_remover::{construct_url, seed};
 // mod web;
 use reqwest::Error;
 
@@ -10,22 +11,11 @@ async fn main() -> Result<(), Error> {
                                                                                     // let res = web::scraper::fetch_url(base_url).await?;
                                                                                     // println!("page: {}", res);
                                                                                     // let table_res = data::categories::create_category_tree(base_url);
-    let table_res = data::categories::seed();
+    let table_res = seed();
+    // let table_res = data::categories::seed();
     match table_res {
         Ok(table) => {
-            // if let Some(subcategory) = find_category_by_url(&table, "better") {
-            //     subcategory.print_full_url();
-            //     if let Ok(Some(parent)) = subcategory.has_parent() {
-            //         println!("found parent: {}", parent.url)
-            //     } else {
-            //         println!("didn't find parent")
-            //     }
-            // } else {
-            //     println!("not found");
-            // }
-            if let Some(url) =
-                data::categories::construct_url("nails", table.get("roofig").expect("REASON"))
-            {
+            if let Some(url) = construct_url("nails", table.get("roofing").expect("REASON")) {
                 println!("url: {}", url)
             } else {
                 println!("not found url for nails")
